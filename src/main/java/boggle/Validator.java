29f6a50f75ee9c166be970f92d79185a56df399c
goldsmithwill <goldsmith.will@gmail.com>
2017-01-27 11:55:00 -0500

    Got getNextIndexes working haven't integrated it into the rest of the code yet

--- src/main/java/boggle/Validator.java
+++ src/main/java/boggle/Validator.java
@@ -4,87 +4,129 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Validator {
 	private char[][] board;
-	private String[] wordArray;
-	private List<String> wordList;
-	private int[] anchorIndex;
-	private int[] searchIndex;
+	private Map<List<int[]>, String> wordPathMap;
+	private Set<String> wordSet;
 
 	// validator constructor starts everything up
-
 	public Validator() throws FileNotFoundException {
 		String filename = "/Users/willgoldsmith/Documents/workspace/boggle-buddy/src/main/resources/words.txt";
 		setWords(filename);
 		setBoard(new char[4][4]);
-		setAnchorIndex(new int[] { 0, 0 });
-		setSearchIndex(new int[] { 0, 0 });
 
 		for (int i = 0; i < getBoard().length; i++) {
 			System.out.println(Arrays.toString(getBoard()[i]));
 		}
 
-		lookup("" + getBoard()[0][0]);
+		//this whole chunk of code is just for testing
+		//it's all hardcoding and stuff
+		List<int[]> tempPath = new ArrayList<int[]>();
+		tempPath.add(new int[] { 1, 1 });
+		getNextIndexes(tempPath);
 	}
 
-	// walkthrough method to walk through board
+	// findWords method to walk through board
+	public void findWords(String word, int[] currentIndex, List<int[]> path) {
 
-	public void walkthrough(String searchWord, String[] validWords) {
+		word += getBoard()[currentIndex[0]][currentIndex[1]];
 
-		lookup(searchWord);
+		if (getWordSet().contains(word)) {
+			getWordPathMap().put(path, word);
+		} else {
+			path.add(currentIndex);
+
+			// getValidNextIndexes(currentIndex, path) return an
+			// int[indexes][x+y]
+			// for loop going through the returned array and running findWords
+			// for each one
+
+			// findWords(word, getNextIndexes(path), path);
+		}
 
 	}
 
-	// lookup method to search if word is in wordList
+	// // validateWord method to search if word is in wordList
+	// public boolean validateWord(String searchWord) {
+	//
+	// // arraylist of valid words that start with searchword
+	// List<String> validWords = new ArrayList<String>();
+	//
+	// // for loop to find all words that start with searchWord
+	// // those words are added to validWords
+	// for (int k = 0; k < getWordArray().length; k++) {
+	// if (getWordArray()[k].startsWith(searchWord)) {
+	// // System.out.println(getWordArray()[k]);
+	// validWords.add(getWordArray()[k]);
+	// }
+	// }
+	//
+	// // if the wordList contains the searchWord
+	// if (getWordSet().contains(searchWord)) {
+	// return false;
+	// }
+	// // if no words start with searchWord
+	// if (validWords.size() == 0) {
+	// return false;
+	// }
+	// // if at least one word starts with searchWord
+	// else {
+	// return true;
+	// }
+	// }
 
-	public void lookup(String searchWord) {
+	// getNextIndexes to return all valid next indexes
+	// for validateWord method
+	public List<int[]> getNextIndexes(List<int[]> path) {
 
-		// arraylist of valid words that start with searchword
-		List<String> validWords = new ArrayList<String>();
+		// getting currentIndex from last element in path
+		int[] currentIndex = path.get(path.size() - 1);
+		System.out.print(Arrays.toString(currentIndex));
 
-		// for loop to find all words that start with searchWord
-		// those words are added to validWords
-		for (int k = 0; k < getWordArray().length; k++) {
-			if (getWordArray()[k].startsWith(searchWord)) {
-				// System.out.println(getWordArray()[k]);
-				validWords.add(getWordArray()[k]);
+		// setting x and y from currentIndex
+		int x = currentIndex[0];
+		int y = currentIndex[1];
+
+		// nextIndexes list to be returned
+		List<int[]> nextIndexes = new ArrayList<int[]>();
+
+		// array w/ all possible indexes touching currentIndex
+		int[][] nextIndexValues = { { x - 1, y }, { x, y - 1 }, { x + 1, y }, { x, y + 1 }, { x - 1, y - 1 },
+				{ x + 1, y - 1 }, { x - 1, y + 1 }, { x + 1, y + 1 } };
+
+		// valid test char to test if the nextIndexValues are valid
+		char validTestChar;
+
+		for (int i = 0; i < 8; i++) {
+
+			// try catch to see which index values are valid or invalid
+			try {
+				// test if nextIndexValues are valid,
+				// using board and validTestChar
+				validTestChar = getBoard()[nextIndexValues[i][0]][nextIndexValues[i][1]];
+
+				// if valid, add the nextIndexValues to nextIndexes
+				nextIndexes.add(new int[] { nextIndexValues[i][0], nextIndexValues[i][1] });
+
+				System.out.println(Arrays.toString(nextIndexValues[i]));
+			} catch (ArrayIndexOutOfBoundsException e) {
+				// if invalid, it doesn't add nextIndexValues to nextIndexes
 			}
+
 		}
 
-		// if the wordList contains the searchWord		
-		if (getWordList().contains(searchWord)) {
-			System.out.println(searchWord);
-			nextAnchor();
-			if (getAnchorY() < 4) {
-				searchWord = Character.toString(getBoard()[getAnchorX()][getAnchorY()]);
-				walkthrough(searchWord, validWords.toArray(new String[validWords.size()]));
-			}
-		
-		
-		}
-		// if no words start with searchWord
-		if (validWords.size() == 0) {
-			nextAnchor();
-
-			if (getAnchorY() < 4) {
-				searchWord = Character.toString(getBoard()[getAnchorX()][getAnchorY()]);
-				walkthrough(searchWord, validWords.toArray(new String[validWords.size()]));
-			}
-		}
-		// if at least one word starts with searchWord
-		else {
-			nextSearchIndex();
-
-			if (getSearchY() < 4) {
-				searchWord += getBoard()[getSearchX()][getSearchY()];
-				walkthrough(searchWord, validWords.toArray(new String[validWords.size()]));
-			}
-		}
+		return nextIndexes;
 	}
+
+	// getters and setters
 
 	public char[][] getBoard() {
 		return board;
@@ -112,7 +154,7 @@ public class Validator {
 		// scanning words.txt file into ArrayList
 		Scanner scanner = new Scanner(new File(filename));
 
-		List<String> lines = new ArrayList<String>();
+		Set<String> lines = new HashSet<String>();
 
 		while (scanner.hasNextLine()) {
 			String nextLine = scanner.nextLine();
@@ -123,81 +165,26 @@ public class Validator {
 		}
 
 		// making array from arraylist
-		setWordList(lines);
-		setWordArray(lines.toArray(new String[0]));
+		setWordSet(lines);
+
+		setWordPathMap(new HashMap<List<int[]>, String>());
 		scanner.close();
 	}
 
-	// methods for currentIndex
-
-	public void nextSearchIndex() {
-		if (getSearchX() + 1 < 4) {
-			setSearchIndex(new int[] { getSearchX() + 1, getSearchY() });
-		} else {
-			setSearchIndex(new int[] { 0, getSearchY() + 1 });
-		}
-
+	public Map<List<int[]>, String> getWordPathMap() {
+		return wordPathMap;
 	}
 
-	public void nextAnchor() {
-		if (getAnchorX() < 3) {
-			setAnchorIndex(new int[] { getAnchorX() + 1, getAnchorY() });
-		} else {
-			setAnchorIndex(new int[] { 0, getAnchorY() + 1 });
-		}
-
-		setSearchIndex(new int[] { 0, 0 });
+	public void setWordPathMap(Map<List<int[]>, String> wordPathMap) {
+		this.wordPathMap = wordPathMap;
 	}
 
-	// methods to get "x" and "y" from index
-
-	public int getAnchorX() {
-		return getAnchorIndex()[0];
+	public Set<String> getWordSet() {
+		return wordSet;
 	}
 
-	public int getAnchorY() {
-		return getAnchorIndex()[1];
+	public void setWordSet(Set<String> wordSet) {
+		this.wordSet = wordSet;
 	}
 
-	public int getSearchX() {
-		return getSearchIndex()[0];
-	}
-
-	public int getSearchY() {
-		return getSearchIndex()[1];
-	}
-
-	// getters and setters
-
-	public String[] getWordArray() {
-		return wordArray;
-	}
-
-	public void setWordArray(String[] wordArray) {
-		this.wordArray = wordArray;
-	}
-
-	public List<String> getWordList() {
-		return wordList;
-	}
-
-	public void setWordList(List<String> wordList) {
-		this.wordList = wordList;
-	}
-
-	public int[] getAnchorIndex() {
-		return anchorIndex;
-	}
-
-	public void setAnchorIndex(int[] anchorIndex) {
-		this.anchorIndex = anchorIndex;
-	}
-
-	public int[] getSearchIndex() {
-		return searchIndex;
-	}
-
-	public void setSearchIndex(int[] searchIndex) {
-		this.searchIndex = searchIndex;
-	}
 }
