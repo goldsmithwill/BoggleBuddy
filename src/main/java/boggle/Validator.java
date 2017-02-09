c284a54733ee6a3ac23d3d3d626ed3d57b2000b1
goldsmithwill <goldsmith.will@gmail.com>
2017-02-09 15:47:08 -0500

    Got the validateWord method working w/ syso print implementation. Yay

--- src/main/java/boggle/Validator.java
+++ src/main/java/boggle/Validator.java
@@ -4,18 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Validator {
 	private char[][] board;
-	private Map<List<int[]>, String> wordPathMap;
-	private Set<String> wordSet;
+	private List<String> wordList;
+	private Set<String> validWordSet;
 
 	// validator constructor starts everything up
 	public Validator() throws FileNotFoundException {
@@ -27,45 +25,103 @@ public class Validator {
 			System.out.println(Arrays.toString(getBoard()[i]));
 		}
 
-		//this whole chunk of code is just for testing
-		//it's all hardcoding and stuff
-		List<int[]> tempPath = new ArrayList<int[]>();
-		tempPath.add(new int[] { 1, 1 });
-		getNextIndexes(tempPath);
+		generateValidWordSet();
+		//
+		// // this whole chunk of code is just for testing
+		// // it's all hardcoding and stuff
+		// List<int[]> tempPath = new ArrayList<int[]>();
+		// tempPath.add(new int[] { 1, 1 });
+		// getNextIndexes(tempPath);
 	}
 
-	// findWords method to walk through board
-	public void findWords(String word, int[] currentIndex, List<int[]> path) {
+	// generateWordSet method to generate a set of
+	// valid words that exist in the board
+	public void generateValidWordSet() {
+		// creating inital nextIndexes list
+		List<int[]> boardIndexes = new ArrayList<int[]>();
 
-		word += getBoard()[currentIndex[0]][currentIndex[1]];
-
-		if (getWordSet().contains(word)) {
-			getWordPathMap().put(path, word);
-		} else {
-			path.add(currentIndex);
-
-			// getValidNextIndexes(currentIndex, path) return an
-			// int[indexes][x+y]
-			// for loop going through the returned array and running findWords
-			// for each one
-
-			// findWords(word, getNextIndexes(path), path);
+		for (int i = 0; i < getBoard().length; i++) {
+			for (int j = 0; j < getBoard()[0].length; j++) {
+				boardIndexes.add(new int[] { i, j });
+			}
 		}
 
+		// for loop to go through wordList
+		for (int k = 0; k < getWordList().size(); k++) {
+			// creating current word
+			String currentWord = getWordList().get(k);
+
+			char[][] boardCopy = new char[4][4];
+			boardCopy = updateToBoard(boardCopy);
+
+			// putting currentWord into recursive validateWord method
+			validateWord(currentWord, boardCopy, boardIndexes, 0);
+		}
 	}
+
+	// boolean recursive validateWord method
+	// to compare input word to board
+	public void validateWord(String searchWord, char[][] board, List<int[]> nextIndexes, int charIndex) {
+		// creating currentChar from charIndex
+		if (charIndex == searchWord.length()) {
+			System.out.println("SEARCHWORD: " + searchWord);
+			return;
+		}
+		
+		char currentChar = searchWord.charAt(charIndex);
+		
+		for (int i = 0; i < nextIndexes.size(); i++) {
+			// creating x and y from current index in nextIndexes
+			int x = nextIndexes.get(i)[0];
+			int y = nextIndexes.get(i)[1];
+
+			// if the currentIndex is currentChar
+			if (board[x][y] == currentChar) {
+				// making board index w/ currentChar blank
+				board[x][y] = ' ';
+
+				// recursion
+				validateWord(searchWord, board, getNextIndexes(new int[] { x, y }), (charIndex + 1));
+
+				// resetting board to global default
+				board = updateToBoard(board);
+			}
+		}
+	}
+
+	// // findWords method to walk through board
+	// public void findWords(String word, int[] currentIndex, List<int[]> path)
+	// {
+	//
+	// word += getBoard()[currentIndex[0]][currentIndex[1]];
+	//
+	// if (getWordSet().contains(word)) {
+	// getWordPathMap().put(path, word);
+	// } else {
+	// path.add(currentIndex);
+	//
+	// // getValidNextIndexes(currentIndex, path) return an
+	// // int[indexes][x+y]
+	// // for loop going through the returned array and running findWords
+	// // for each one
+	//
+	// // findWords(word, getNextIndexes(path), path);
+	// }
+	//
+	// }
 
 	// // validateWord method to search if word is in wordList
 	// public boolean validateWord(String searchWord) {
 	//
 	// // arraylist of valid words that start with searchword
-	// List<String> validWords = new ArrayList<String>();
+	// List<String> validWordSet = new ArrayList<String>();
 	//
 	// // for loop to find all words that start with searchWord
-	// // those words are added to validWords
+	// // those words are added to validWordSet
 	// for (int k = 0; k < getWordArray().length; k++) {
 	// if (getWordArray()[k].startsWith(searchWord)) {
 	// // System.out.println(getWordArray()[k]);
-	// validWords.add(getWordArray()[k]);
+	// validWordSet.add(getWordArray()[k]);
 	// }
 	// }
 	//
@@ -74,7 +130,7 @@ public class Validator {
 	// return false;
 	// }
 	// // if no words start with searchWord
-	// if (validWords.size() == 0) {
+	// if (validWordSet.size() == 0) {
 	// return false;
 	// }
 	// // if at least one word starts with searchWord
@@ -85,11 +141,7 @@ public class Validator {
 
 	// getNextIndexes to return all valid next indexes
 	// for validateWord method
-	public List<int[]> getNextIndexes(List<int[]> path) {
-
-		// getting currentIndex from last element in path
-		int[] currentIndex = path.get(path.size() - 1);
-		System.out.print(Arrays.toString(currentIndex));
+	public List<int[]> getNextIndexes(int[] currentIndex) {
 
 		// setting x and y from currentIndex
 		int x = currentIndex[0];
@@ -102,28 +154,36 @@ public class Validator {
 		int[][] nextIndexValues = { { x - 1, y }, { x, y - 1 }, { x + 1, y }, { x, y + 1 }, { x - 1, y - 1 },
 				{ x + 1, y - 1 }, { x - 1, y + 1 }, { x + 1, y + 1 } };
 
-		// valid test char to test if the nextIndexValues are valid
-		char validTestChar;
+		for (int i = 0; i < nextIndexValues.length; i++) {
 
-		for (int i = 0; i < 8; i++) {
+			// if statement to make sure no values in the index are
+			// smaller than 0 or bigger than 3
+			// (aka breaking the boundaries of the board)
+			if ((nextIndexValues[i][0] >= 0 && nextIndexValues[i][0] <= 3)
+					&& (nextIndexValues[i][1] >= 0 && nextIndexValues[i][1] <= 3)) {
 
-			// try catch to see which index values are valid or invalid
-			try {
-				// test if nextIndexValues are valid,
-				// using board and validTestChar
-				validTestChar = getBoard()[nextIndexValues[i][0]][nextIndexValues[i][1]];
-
-				// if valid, add the nextIndexValues to nextIndexes
+				// if the values aren't breaking boundaries, add to nextIndexes
 				nextIndexes.add(new int[] { nextIndexValues[i][0], nextIndexValues[i][1] });
-
-				System.out.println(Arrays.toString(nextIndexValues[i]));
-			} catch (ArrayIndexOutOfBoundsException e) {
-				// if invalid, it doesn't add nextIndexValues to nextIndexes
+				// System.out.print(Arrays.toString(new int[] {
+				// nextIndexValues[i][0], nextIndexValues[i][1] }));
 			}
 
 		}
 
 		return nextIndexes;
+
+	}
+
+	// method updating passed in value to board field values
+	public char[][] updateToBoard(char[][] boardCopy) {
+		boardCopy = new char[getBoard().length][getBoard()[0].length];
+		for (int m = 0; m < getBoard().length; m++) {
+			for (int n = 0; n < getBoard()[0].length; n++) {
+				boardCopy[m][n] = getBoard()[m][n];
+			}
+		}
+		return boardCopy;
+
 	}
 
 	// getters and setters
@@ -148,13 +208,14 @@ public class Validator {
 				this.board[i][j] = (char) letterNum;
 			}
 		}
+
 	}
 
 	public void setWords(String filename) throws FileNotFoundException {
 		// scanning words.txt file into ArrayList
 		Scanner scanner = new Scanner(new File(filename));
 
-		Set<String> lines = new HashSet<String>();
+		List<String> lines = new ArrayList<String>();
 
 		while (scanner.hasNextLine()) {
 			String nextLine = scanner.nextLine();
@@ -165,26 +226,26 @@ public class Validator {
 		}
 
 		// making array from arraylist
-		setWordSet(lines);
+		setWordList(lines);
 
-		setWordPathMap(new HashMap<List<int[]>, String>());
+		setvalidWordSet(new HashSet<String>());
 		scanner.close();
 	}
 
-	public Map<List<int[]>, String> getWordPathMap() {
-		return wordPathMap;
+	public List<String> getWordList() {
+		return wordList;
 	}
 
-	public void setWordPathMap(Map<List<int[]>, String> wordPathMap) {
-		this.wordPathMap = wordPathMap;
+	public void setWordList(List<String> wordList) {
+		this.wordList = wordList;
 	}
 
-	public Set<String> getWordSet() {
-		return wordSet;
+	public Set<String> getvalidWordSet() {
+		return validWordSet;
 	}
 
-	public void setWordSet(Set<String> wordSet) {
-		this.wordSet = wordSet;
+	public void setvalidWordSet(Set<String> validWordSet) {
+		this.validWordSet = validWordSet;
 	}
 
 }
